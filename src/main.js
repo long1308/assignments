e1c15047b13fced924650935f11be68d4d5cd382
Do Duy Long <103615878+long1308@users.noreply.github.com>
2023-02-13 23:47:36 +0700

    feat: add new base assignment

--- src/main.js
+++ src/main.js
@@ -1,1 +1,15 @@
-console.log(1);
+// bootstrap
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.js";
+// css module
+import classes from "./main.module.css";
+
+import { router, render } from "./utilities";
+
+const app = document.querySelector("#app");
+
+//pages
+import HomePage from "./pages/HomePage";
+
+router.on("/", () => render(HomePage, app));
+ router.resolve();
