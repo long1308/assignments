11dc4bbc5e95b77018e67008ec9c64d2a71f5060
Do Duy Long <103615878+long1308@users.noreply.github.com>
2023-02-17 21:04:41 +0700

    fix: add tailwwin

--- src/components/Pages/Project.js
+++ src/components/Pages/Project.js
@@ -28,7 +28,7 @@ const Project = () => {
                         ${categorys
                           .map(
                             (category) =>
-                              /*html*/ `<button type="button" data-id = "${category.id}" class="btn-project text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2" >${category.name}</button>`
+                              /*html*/ `<button type="button" data-id = "${category.id}" class=" btn-project text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2" >${category.name}</button>`
                           )
                           .join(" ")}
                     </div>
@@ -38,7 +38,7 @@ const Project = () => {
                         .map(
                           (item) => /*html*/ `
                           
-                          <div class="flex-wrap mr-7 shadow-boxShadowNew">
+                          <div class="flex-wrap mr-7 shadow-boxShadowNew ">
                             <a class="relative block  bg-gray-900 group w-72 rounded-lg p-4" href="##">
                               <img
                                 class="absolute inset-0 object-cover w-full h-full group-hover:opacity-50"
@@ -47,7 +47,7 @@ const Project = () => {
                               <div class="relative p-5">
                                 <div class="mt-40">
                                   <div
-                                    class="transition-all duration-700 transform translate-y-8 opacity-0 group-hover:opacity-100 group-hover:translate-y-0"
+                                    class="transition-all duration-1000 transform translate-y-8 opacity-0 group-hover:opacity-100 group-hover:translate-y-0"
                                   >
                                     <div class="p-2">
                                       <p class="text-xl text-yellow-500 font-bold italic">${item.name}</p>
---
+++ tailwind.config.cjs
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  // prefix: "tw-",
+  theme: {
+    extend: {
+      boxShadow: {
+        boxShadowNew: "rgba(149, 157, 165, 0.2) 0px 8px 24px;",
+      },
+    },
+  },
+  plugins: [],
+};
