df5e2c14f053a403700ac443f2fd5287a5a3f2fd
Do Duy Long <103615878+long1308@users.noreply.github.com>
2023-02-20 15:13:57 +0700

    refacter : home

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -5,7 +5,7 @@ const Home = () => {
     <div class ="container text-center">
         <img class ="blok m-auto rounded-full w-48 h-48"src="https://scontent-hkg4-1.xx.fbcdn.net/v/t39.30808-6/274727368_540288330979926_3385754132753723765_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=174925&_nc_ohc=I_VWYcJ7y0oAX-_PPuz&_nc_ht=scontent-hkg4-1.xx&oh=00_AfA2pYyH-o9wIoMOHXO3xhWpEyysmZbSpSmZlT4upQt1uA&oe=63F203E8" alt="">
         <h1 class ="text-3xl my-3 tracking-widest font-black italic">Do Duy Long</h1>
-        <span class ="font-normal">I'm a Front-End developer
+        <span class ="font-normal">I'm a Front-End developer 💗
         </span>
         <nav class ="flex items-center justify-center ">
             <ul class=" mb-0 mt-4 flex">
